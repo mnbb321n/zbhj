a7aedfbca6a22ce8de0b1d63a6cabd413871ddf9
mnbb321n <96606377+mnbb321n@users.noreply.github.com>
2021-12-24 16:03:20 +0800

    Update zqydCOOKIE.js

--- zqydCOOKIE.js
+++ zqydCOOKIE.js
@@ -61,7 +61,7 @@ module.exports = {
     "datas": [{
       "key": "zqydbody",
       "val": ""
-       let zqydbody= $.isNode() ? (process.env.zqydbody ? process.env.zqydbody : "") : ($.getdata('zqydbody') ? $.getdata('zqydbody') : "") 
+       
     }],
     "sessions": [],
     "isLoaded": true
