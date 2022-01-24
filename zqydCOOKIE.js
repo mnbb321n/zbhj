6a2a56e943f3d5d6090e714bdc3d3ee86521f2fb
mnbb321n <96606377+mnbb321n@users.noreply.github.com>
2022-01-24 23:16:00 +0800

    Update zqydCOOKIE.js

--- zqydCOOKIE.js
+++ zqydCOOKIE.js
@@ -1,7 +1,7 @@
 module.exports = {
     "id": "zqyd",
     "name": "钟情阅读",
-    "keys": ["zqydbody","zqydbody2","zqydbody3","zqydbody4","zqydbody5","zqydbody6","zqydbody7",],
+    "keys": ["zqydbody2","zqydbody4","zqydbody","zqydbody3","zqydbody5","zqydbody6","zqydbody7",],
     "author": "@tom",
     "settings": [{
       "id": "zqydSuffix",
@@ -65,10 +65,10 @@ module.exports = {
       "key": "zqydbody4",
       "val": process.env.zqydbody4
     }, {
-      "key": "zqydbody3",
+      "key": "zqydbody1",
       "val": process.env.zqydbody3
     }, {
-      "key": "zqydbody4",
+      "key": "zqydbody3",
       "val": process.env.zqydbody4
     }, {
       "key": "zqydbody5",
